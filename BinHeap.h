bb68b81a5b01cb0de353c4e1196ef60361b84924
AnastasiaKoshel <32370500+AnastasiaKoshel@users.noreply.github.com>
2018-03-04 18:41:54 +0200

    Add files via upload

---
+++ BinHeap.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef BH_h
+#define BH_h
+
+struct node
+{
+	int data;
+	int degree;
+	struct node *child;
+	struct node *sibling;
+	struct node *parent;
+};
+
+class binheap
+{
+private:
+	
+	node *merge(node *root1, node *root2);
+	node *fix(node *root);
+	void join(node *root1, node *root2);
+public:
+	node *head;
+	binheap()
+	{
+		head = NULL;
+	}
+	node *createnode(int  key);
+	void insert(node *root);
+	void display(node *h);
+	int printmin();
+	int printheadnode();
+	node *extractmin();
+	void getmin(node *&minimum);
+	bool found(node *root, int key);
+	bool search(node *root, int key, node *&temp);
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,78 @@
+// BinHeap.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "BinHeap.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	binheap bh;
+	node *root=NULL;
+	int n,x;
+			cout << endl << "Write the number of elements"<<endl;
+			cin >> n;
+			for (int i = 0; i < n; i++)
+			{
+				x = 1 + rand() % 100;
+				cout << x << " ";
+				root = bh.createnode(x);
+				bh.insert(root);
+			}
+        cout << "Min =" << endl;
+			x = bh.printmin();
+			if (x == INT_MAX)
+				cout << "None" << endl;
+			else cout << endl << x << endl;
+		
+			cout << "HeadNode" << endl;
+			x = bh.printheadnode();
+			if (x == INT_MAX)
+				cout << "Empty" << endl;
+			else cout << endl << x << endl;
+			cout << "Heap" << endl;
+			bh.display(bh.head);
+			cout << endl << "Write key to be deleted\t";
+			cin >> x;
+			if (!bh.found(bh.head, x))
+				cout << endl << "Not found" << endl;
+			else
+				bh.extractmin();
+			cout << "Heap" << endl;
+			bh.display(bh.head);
+			cout << "Extract min" << endl;
+			bh.extractmin();
+			cout << "Heap" << endl;
+			bh.display(bh.head);
+			cout << endl << "Enter key to be decreased\t";
+			cin >> x;
+			if (!bh.search(bh.head, x, root))
+				cout << endl << "Not found" << endl;
+			else
+			{
+				cout << "Enter data to replaced\t";
+				cin >> x;
+				cout << endl;
+				root->data = x;
+				while (root->parent && root->data<root->parent->data)
+				{
+					x = root->data;
+					root->data = root->parent->data;
+					root->parent->data = x;
+					root = root->parent;
+				}
+				while (root->child && root->child->data<root->data)
+				{
+					x = root->data;
+					root->data = root->child->data;
+					root->child->data = x;
+					root = root->child;
+				}
+			}
+			
+			
+			system("pause");
+    return 0;
+}
+
